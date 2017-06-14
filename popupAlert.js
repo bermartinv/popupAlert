195d37096abd10593962c187aa81c1e2103a7dc5
unknown <bermartinv@gmail.com>
2017-06-15 00:27:08 +0200

    Convertido a libreria

--- popupAlert.js
+++ popupAlert.js
@@ -1,4 +1,4 @@
-var modal = document.getElementById('myModal');
+/*var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
@@ -14,11 +14,38 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
+}*/
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+/*window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}*/
+var popUp = {
+	modal : document.getElementById('myModal'),
+	btn : document.getElementById("myBtn"),
+	span : document.getElementsByClassName("close")[0],
+	boton :  function(){
+		popUp.btn.addEventListener('click',function() {
+    			popUp.modal.style.display = "block";
+		})
+	},
+	spanBoton : function(){
+		popUp.span.addEventListener('click',function(){
+				popUp.modal.style.display = "none";	
+		})
+	},
+	windowClick : function(){
+		window.onclick = function(event){
+			if (event.target == popUp.modal){
+				popUp.modal.style.display = "none";
+			}
+		}
+	}
+	
 }
+popUp.boton();
+popUp.spanBoton();
+popUp.windowClick();
+
